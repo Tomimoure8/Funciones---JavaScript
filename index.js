05f3b84ac21b84066556d2bc7e986af6cdd9a816
tomimoure8 <tomimoure8@gmail.com>
2024-12-18 13:57:51 -0300

    resolucion-calcular-el-area-de-un-rectangulo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Declaraciòn de variable para guardar el resultado de la suma
-let tomi = function (a, b) {return a + b}
-let resultado = alert (tomi (3,7));
-let mai = function (a, b) {return a - b}
-let resultadoo = alert(mai(23,1));
+// let tomi = function (a, b) {return a + b}
+// let resultado = alert (tomi (3,7));
+// let mai = function (a, b) {return a - b}
+// let resultadoo = alert(mai(23,1));
 
 
 
@@ -10,6 +10,20 @@ let resultadoo = alert(mai(23,1));
 // CLASE 4: Funciones
 
 // Actividades:
+
+// Actividad de repaso: Calcular el area de un rectangulo // Crea una función en JavaScript que calcule el área de un triángulo y almacene el resultado en una variable global. La función debe tomar dos parámetros: la base y la altura del triángulo. Luego, llama a la función con algunos valores de prueba y muestra el resultado almacenado en la variable global en la consola.
+
+let areaTriangulo;
+
+function calcularAreaTriangulo (base, altura) {
+    return (base * altura) / 2;
+}
+
+areaTriangulo = calcularAreaTriangulo (6, 4);
+
+console.log (areaTriangulo);
+
+// Actividad de repaso: Goles del campeon 🏆⚽ // Escribí una función en JavaScript que calcule la cantidad total de goles marcados por un jugador en una temporada y almacene el resultado en una variable global. La función debe tomar como parámetros la cantidad de partidos jugados y el promedio de goles por partido. Luego, muestra el resultado almacenado en la variable global en la consola.
 
 
 
